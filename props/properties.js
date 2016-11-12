408ff59aac85bfee87ae028a1a8932efd7b70ea1
tthinh@gmail.com <tthinh@gmail.com>
2016-11-11 23:20:21 -0800

    add properties

---
+++ props/properties.js
@@ -0,0 +1,24 @@
+title="JNL Cable"
+
+section1_title="JNL CABLE"
+section1_description="Custom cables manufactured with top quality care and precision.  Delivering the products you need with quick turn around and our service guarantee, resulting in lower overall costs and higher value."
+section1_link="Find Out More"
+
+section2_title="We've got what you need!"
+section2_description="JNL Cable has everything you need to get your new wires and cables connected in no time! Fast and reliable service."
+section2_link="Get Started!"
+
+section3_title="At Your Service"
+section3_col1_title="Quick Service"
+section3_col1_description="Lightning fast product."
+section3_col2_title="Reliable Cost"
+section3_col2_description="Quality parts at the fraction of the cost."
+section3_col3_title="Effective"
+section3_col3_description="Effective use for any product."
+section3_col4_title="High Quality"
+section3_col4_description="Made to handle it all."
+section3_link="Contact us"
+
+section4_title="Let's Get In Touch!"
+section4_description="Let's Get In Touch!"
+section4_link="Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!"
